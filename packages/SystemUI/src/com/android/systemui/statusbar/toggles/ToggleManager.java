c484a534a42725e60a8dd229e05f86dc8ea3dc3f
Bajee11 <bajee11@gmail.com>
2013-01-26 20:26:29 -0500

    Add Profile Tile
    Conflicts:
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
    	policy/src/com/android/internal/policy/impl/GlobalActions.java
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    Change-Id: I359fc5cb8794c48bb27b6e2d0acbff1eadfa413a

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
@@ -96,6 +96,7 @@ public class ToggleManager {
     public static final String CROM_TOGGLE = "CROM";
     public static final String TRDS_TOGGLE = "TRDS";
     public static final String POWERMENU_TOGGLE = "POWERMENU";
+    public static final String PROFILE_TOGGLE = "PROFILE";
     public static final String SCREENSHOT_TOGGLE = "SCREENSHOT";
     public static final String CUSTOM_TOGGLE = "CUSTOM";
     public static final String STAYAWAKE_TOGGLE = "STAYAWAKE";
@@ -164,6 +165,7 @@ public class ToggleManager {
             toggleMap.put(CROM_TOGGLE, CRoMToggle.class);
             toggleMap.put(TRDS_TOGGLE, TRDSToggle.class);
             toggleMap.put(POWERMENU_TOGGLE, PowerMenuToggle.class);
+            toggleMap.put(PROFILE_TOGGLE, ProfileToggle.class);
             toggleMap.put(SCREENSHOT_TOGGLE, ScreenshotToggle.class);
             toggleMap.put(CUSTOM_TOGGLE, CustomToggle.class);
             toggleMap.put(STAYAWAKE_TOGGLE, StayAwakeToggle.class);
