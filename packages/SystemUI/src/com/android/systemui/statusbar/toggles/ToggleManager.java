23a97fe6464ab96465d1548155b05e3929280e01
slickrick <rsalinas1@mail.usf.edu>
2013-03-23 01:35:57 -0400

    Hybrid Settings toggle (1/2)

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
@@ -90,6 +90,7 @@ public class ToggleManager {
     public static final String TRDS_TOGGLE = "TRDS";
     public static final String POWERMENU_TOGGLE = "POWERMENU";
     public static final String XPOSED_TOGGLE = "XPOSED";
+    public static final String HYBRID_TOGGLE = "HYBRID";
 
     private int mStyle;
 
@@ -143,6 +144,7 @@ public class ToggleManager {
             toggleMap.put(TRDS_TOGGLE, TRDSToggle.class);
             toggleMap.put(POWERMENU_TOGGLE, PowerMenuToggle.class);
             toggleMap.put(XPOSED_TOGGLE, XposedToggle.class);
+            toggleMap.put(HYBRID_TOGGLE, HybridToggle.class);
             // toggleMap.put(BT_TETHER_TOGGLE, null);
         }
         return toggleMap;
