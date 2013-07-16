68540dd35b330dc5de4ff4f4b77626f5715748a2
Der-Schubi <schubi@erlangen.ccc.de>
2013-07-16 11:28:59 -0300

    Option to enable Notification Light when screen is on (1/2) This makes possible to enable the Notification LED even when the screen is on
    Change-Id: I78609bfc7429310a209808939de0286999bb7be0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3906,6 +3906,15 @@ public final class Settings {
          */
         public static final String HALO_CIRCLE_COLOR = "halo_circle_color";
 
+	/**
+         * Screen-On Notification Light,
+         * should default to 1 (yes, Notification Light is enabled when screen is on)
+         * @hide
+         */
+        public static final String SCREEN_ON_NOTIFICATION_LED = "screen_on_notification_led";
+
+
+
         /**
          * Swap volume buttons when the screen is rotated by 90 or 180 degrees
          * @hide
